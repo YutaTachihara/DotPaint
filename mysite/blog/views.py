7262b947e649d4c4c1d3e43941ced539efe1c458
Yuta Tachihara <>
2020-01-22 20:06:43 +0900

    [Add] header

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,6 @@ from django.views import generic
 
 from .models import Article
 
-
 class IndexView(generic.ListView):
     template_name = 'blog/index.html'
     context_object_name = 'latest_articles'
