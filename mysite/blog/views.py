09464bab75bfba657be1d6487e0dafb61dcece45
Yuta Tachihara <>
2020-01-22 17:25:00 +0900

    [Add] override get_queryset

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,11 @@ from .models import Article
 
 class IndexView(generic.ListView):
     template_name = 'blog/index.html'
+    context_object_name = 'latest_articles'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Article.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
