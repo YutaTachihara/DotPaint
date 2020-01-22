0d634dccecb443394d87c54e9c45bc5f8310cc6a
Yuta Tachihara <>
2020-01-22 17:19:37 +0900

    [Update] use generic template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
 from .models import Article
 
 
-def index(request):
-    return render(request, 'blog/index.html')
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
 
-def detail(request, article_id):
-    # question = get_object_or_404(Question, pk=question_id)
-    # return render(request, 'polls/detail.html', {'question': question})
-    return HttpResponse("Detail")
+
+class DetailView(generic.DetailView):
+    model = Article
+    template_name = 'blog/detail.html'
