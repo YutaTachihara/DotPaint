d9e2f9203d70e71f941dc119508fe1043bdc83a5
Yuta Tachihara <>
2020-01-22 17:14:19 +0900

    [Add] namespace

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Article
 
@@ -8,4 +8,6 @@ def index(request):
     return render(request, 'blog/index.html')
 
 def detail(request, article_id):
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request, 'polls/detail.html', {'question': question})
     return HttpResponse("Detail")
