0718c3d3fd3a7579a1c8f13ae3cd2d433ae50097
Yuta Tachihara <>
2020-01-22 16:55:59 +0900

    [Add] Detail method.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Index")
+
+def detail(request, article_id):
+    return HttpResponse("Detail")
