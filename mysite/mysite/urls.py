e073aef1d00269a67db7b5059ac9d117637f6d39
Yuta Tachihara <>
2020-01-25 08:39:13 +0900

    setup login

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
+    path('tubuyakkii/', include('tsubuyakkii.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
